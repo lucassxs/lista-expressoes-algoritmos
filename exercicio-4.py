a1b4e5d6721002c3433fb82832bd1f5ca97323ec
Lucas Stefano <lucassxs.sp@gmail.com>
2021-02-08 13:53:03 -0300

    add exercicio-3

---
+++ exercicio-4.py
@@ -0,0 +1,8 @@
+# # Faça um algoritmo que receba 4 notas, calcular e informar a média ponderada, considerando que os pesos são respectivamente 2,2,3,3
+
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota: '))
+n3 = int(input('Digite a terceira nota: '))
+n4 = int(input('Digite a quarta nota: '))
+resultado = ((n1*2 + n2*2 + n3*3 + n4*3)/(2 + 2 + 3 + 3))
+print('O resultado da média ponderada entre {}, {}, {} e {}, é: {}!'.format(n1, n2, n3, n4, resultado))
