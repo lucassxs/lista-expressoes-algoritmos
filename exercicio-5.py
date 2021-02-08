a273667ec4436c16b3ef82e433b25bab56cbc7bc
Lucas Stefano <lucassxs.sp@gmail.com>
2021-02-08 13:56:09 -0300

    add exercicio-6

--- exercicio-5.py
+++ exercicio-5.py
@@ -1,6 +1,6 @@
 # Escreva um programa que solicite ao usuário o raio(em centímetros), de uma circunferência e apresente a ele o diâmetro, a área e o perímetro da respectiva cincunferência.
 
-# Cálculo do diâmetro
+# Cálculo do diâm
 r = int(input('Digite um valor para o raio: '))
 d = (2*r)
 print('O valor do diâmetro da respectiva circunferência para um raio de {}, é: {}!'.format(r, d))
