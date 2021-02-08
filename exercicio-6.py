2e0b5c9e743e17a971d2f03ca4b74a9bfda05f1a
Lucas Stefano <lucassxs.sp@gmail.com>
2021-02-08 13:57:16 -0300

    add exercicio-7

---
+++ exercicio-6.py
@@ -0,0 +1,7 @@
+# Elaborar um algoritmo para calcular o volume de um cilindro
+
+r = int(input('Digite um valor para o raio '))
+h = int(input('Digite um valor para a altura: '))
+PI = 3.14
+v = (PI*r**2*h)
+print('O valor do volume do cilindro é: {}!'.format(v))
---
+++ exercicio-7.py
@@ -0,0 +1,6 @@
+# Crie um programa que solicite e apresente os valores da diagonal maior e menor de um losango e calcule a área.
+
+d1 = int(input('Digite um valor para a diagonal maior: '))
+d2 = int(input('Digite um valor para a diagonal menor:'))
+a = (d1 * d2)/2
+print('O valor da área de um losango é: {}!'.format(a))
