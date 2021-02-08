e9cf5f0e60831c8069e6c45f1734c815063d7692
Lucas Stefano <lucassxs.sp@gmail.com>
2021-02-08 13:51:49 -0300

    add exercio-3

---
+++ exercicio-3.py
@@ -0,0 +1,7 @@
+# Faça um algoritmo que receba três números, calcule e mostre a média aritmética entre eles
+
+a = int(input('Digite a variável a: '))
+b = int(input('Digite a variável b: '))
+c = int(input('Digite a variável c: '))
+resultado = ((a + b + c)/3)
+print('O resultado da média entre {}, {} e {} é {}!'.format(a, b, c, resultado))
