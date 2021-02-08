f9939c0bbd302f249d1a6636e50410e51c6083e2
Lucas Stefano <lucassxs.sp@gmail.com>
2021-02-08 14:05:23 -0300

    add exercicio-12

---
+++ exercicio-12.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba um número real e apresente o valor de sus raiz quadrada.
+
+import math
+n = float(input('digite um número para extrairmos a raiz quadrada: '))
+r = math.sqrt(n)
+print('o valor da raiz quadrada do número é: {}!'.format(r))
